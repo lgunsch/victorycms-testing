b11dfd7f23a87704491ee944bca326b4d92be462
Mitchell Bosecke <mitchell.bosecke@greybox.co>
2011-02-17 21:19:58 -0700

    Added unit tests but didn't implement them

--- lib/test/core/VcmsTesting-LibraryLoaderTest.php
+++ lib/test/core/VcmsTesting-LibraryLoaderTest.php
@@ -37,27 +37,9 @@ class LibraryLoaderTest extends UnitTestCase
 	/*
 	 * Create a temporary directory with an external library.
 	 */
-	public function setupTempDir()
+	public function setupTempLibrary()
 	{
-		// get a temporary unique name
-		$tempName = tempnam(sys_get_temp_dir(), 'libraryloader_test');
-		if ($tempName === false) {
-			exit('Could not create a temporary directory for testing.');
-		}
-		
-		// Delete the created unique file.
-		$result = unlink($tempName);
-		if ($result === false) {
-			exit('Could not create a temporary directory for testing.');
-		}
-		
-		// Use the unique name to create a unique directory
-		$result = mkdir($tempName, 0777, true);
-		if ($result === false) {
-			exit('Could not create a temporary directory for testing.');
-		}
-		
-		$this->tempDir = $tempName;
+		//TODO: Implement
 	}
 	
 	/*
@@ -65,10 +47,7 @@ class LibraryLoaderTest extends UnitTestCase
 	 */
 	public function removeTempDir()
 	{
-		$result = rmdir($this->tempDir);
-		if ($result === false) {
-			exit("Could not delete the temporary directory for testing.\n");
-		}
+		//TODO: Implement
 	}
 	
 	/*
@@ -76,10 +55,7 @@ class LibraryLoaderTest extends UnitTestCase
 	 */
 	public function testInstance()
 	{
-		$libraryloader = LibraryLoader::getInstance();
-		$this->assertIsA($libraryloader, 'Vcms\LibraryLoader');
-		$libraryloader2 = $libraryloader;
-		$this->assertReference($libraryloader, $libraryloader2, 'Copy references are different');
+		//TODO: Implement
 	}
 	
 	/*
@@ -87,318 +63,50 @@ class LibraryLoaderTest extends UnitTestCase
 	 */
 	public function testClone()
 	{
-		$autoloader = Autoloader::getInstance();
-		try {
-			$autoloader2 = clone $autoloader;
-			$this->fail('Did not throw an exception when cloning');
-		} catch (Vcms\Exception\SingletonCopyException $e) {}
+		//TODO: Implement
 	}
 	
 	/*
-	 * Test loading the external library
+	 * Test loading an external library
 	 */
-	public function testAddListDirs()
+	public function testLibrary()
 	{
-		$this->setupTempDir();
-		
-		// AutoLoaderMock will check the paths here so we
-		// have to create valid directories
-		$top = $this->tempDir;
-		$path1 = Autoloader::truepath($top.'/a');
-		$path2 = Autoloader::truepath($top.'/b');
-		
-		$this->assertTrue(mkdir($path1, 0777, true));
-		$this->assertTrue(mkdir($path2, 0777, true));
-		
-		// test after instantiation
-		$this->assertIdentical(array(), Autoloader::listDirs());
-		
-		// Regular directory adding
-		AutoloaderMock::addDir($path1);
-		$this->assertIdentical(array($path1), Autoloader::listDirs());
-		$this->assertIdentical(array($path1), Registry::get(RegistryKeys::autoload));
-		
-		AutoloaderMock::addDir($path2);
-		$this->assertIdentical(array($path1, $path2), Autoloader::listDirs());
-		$this->assertIdentical(
-			array($path1, $path2),
-			Registry::get(RegistryKeys::autoload)
-		);
-		
-		// test bad directory
-		try {
-			AutoloaderMock::addDir(array('bad!'));
-			$this->fail("Should not be able to add an array.");
-		} catch (\Vcms\Exception\DataTypeException $e) {}
-		
-		// test empty directory
-		try {
-			AutoloaderMock::addDir('');
-			$this->fail("Should not be able to add an empty string.");
-		} catch (\Vcms\Exception\DataTypeException $e) {}
-		
-		$this->assertTrue(rmdir($path1));
-		$this->assertTrue(rmdir($path2));
-		$this->removeTempDir();
+		//TODO: implement
 	}
 	
 	/*
-	 * Test single namespace only regular expression pattern.
+	 * Test loading an external library with a config file
 	 */
-	public function testSinglePattern()
+	public function testLibraryWithConfig()
 	{
-		$patterns = array(
-			AutoloaderMock::returnPattern('Vcms\FileUtils'),
-			AutoloaderMock::returnPattern('\Vcms\FileUtils')
-		);
-		
-		// Test valid matches
-		$fileNames = array(
-			'vcms.fileutils', // all lower-case
-			'vcms-fileutils',
-			'vcms...fileutils',
-			'vcms--fileutils',
-			'Vcms.FileUtils', // Matching case
-			'Vcms-FileUtils',
-			'Vcms...FileUtils',
-			'Vcms--FileUtils',
-			'vCms.fileuTiLs', // improper case
-			'vCms-fileuTiLs',
-			'vCms...fileuTiLs',
-			'vCms--fileuTiLs',
-		);
-		
-		$size = count($fileNames);
-		for ($i = 0; $i < $size; $i++) {
-			// make even more valid combinations
-			array_push($fileNames, 'class.'.$fileNames[$i]);
-			array_push($fileNames, $fileNames[$i].'.inc');
-			array_push($fileNames, 'class.'.$fileNames[$i].'.inc');
-			array_push($fileNames, $fileNames[$i].'.class');
-			array_push($fileNames, 'class.'.$fileNames[$i].'.class');
-			array_push($fileNames, $fileNames[$i].'.inc.class');
-			array_push($fileNames, 'class.'.$fileNames[$i].'.inc.class');
-			array_push($fileNames, $fileNames[$i].'.class.inc');
-			array_push($fileNames, 'class.'.$fileNames[$i].'.class.inc');
-		}
-		
-		foreach ($fileNames as $fileName) {
-			//echo "Matching: $fileName\n";
-			foreach($patterns as $pattern) {
-				$num = preg_match($pattern, $fileName);
-				$this->assertIdentical(1, $num, "Should match $fileName");
-			}
-		}
-		
-		// Test non-matching
-		$badNames = array(
-			'.Vcms.fileutils', // all lower-case
-			'.Vcms-fileutils',
-			'vcmsfileutils',
-			'vcms\fileutils',
-			'vcms::fileutils',
-			'vcms_fileutils',
-			'vcms fileutils',
-			'vcms 	 fileutils',
-			'.Vcms.FileUtils', // Matching case
-			'.Vcms-FileUtils',
-			'VcmsFileUtils',
-			'Vcms\FileUtils',
-			'Vcms::FileUtils',
-			'Vcms_FileUtils',
-			'Vcms FileUtils',
-			'Vcms 	 FileUtils'
-		);
-		
-		foreach ($badNames as $badName) {
-			foreach($patterns as $pattern) {
-				$num = preg_match($pattern, $badName);
-				$this->assertIdentical(0, $num, "Should not match $badName");
-			}
-		}
+		//TODO: implement
 	}
 	
 	/*
-	 * Test multiple sub-namespace regular expression patterns.
+	 * Test loading an external library that doesn't exist.
 	 */
-	public function testMultiPattern()
+	public function testNonExistingLibrary()
 	{
-		$patterns = array(
-			AutoloaderMock::returnPattern('Vcms\SubOne\SubTwo\FileUtils'),
-			AutoloaderMock::returnPattern('\Vcms\SubOne\SubTwo\FileUtils')
-		);
-			
-		// Test valid matches
-		$fileNames = array(
-			'vcms.subone.subtwo.fileutils', // all lower-case
-			'vcms-subone-subtwo-fileutils',
-			'vcms...subone...subtwo...fileutils',
-			'vcms--subone--subtwo--fileutils',
-			'Vcms.SubOne.SubTwo.FileUtils', // Matching case
-			'Vcms-SubOne-SubTwo-FileUtils',
-			'Vcms...SubOne...SubTwo...FileUtils',
-			'Vcms--SubOne--SubTwo--FileUtils',
-			'vCms.sUboNe.sUbtWo.fileuTiLs', // improper case
-			'vCms-sUboNe-sUbtWo-fileuTiLs',
-			'vCms...sUboNe...sUbtWo...fileuTiLs',
-			'vCms--sUboNe--sUbtWo--fileuTiLs',
-		);
-		
-		$size = count($fileNames);
-		for ($i = 0; $i < $size; $i++) {
-			// make even more valid combinations
-			array_push($fileNames, 'class.'.$fileNames[$i]);
-			array_push($fileNames, $fileNames[$i].'.inc');
-			array_push($fileNames, 'class.'.$fileNames[$i].'.inc');
-			array_push($fileNames, $fileNames[$i].'.class');
-			array_push($fileNames, 'class.'.$fileNames[$i].'.class');
-			array_push($fileNames, $fileNames[$i].'.inc.class');
-			array_push($fileNames, 'class.'.$fileNames[$i].'.inc.class');
-			array_push($fileNames, $fileNames[$i].'.class.inc');
-			array_push($fileNames, 'class.'.$fileNames[$i].'.class.inc');
-		}
-		
-		foreach ($fileNames as $fileName) {
-			//echo "Matching: $fileName\n";
-			foreach($patterns as $pattern) {
-				$num = preg_match($pattern, $fileName);
-				$this->assertIdentical(1, $num, "Should match $fileName");
-			}
-		}
-		
-		// Test non-matching
-		$badNames = array(
-			'.Vcms.subone.subtwo.fileutils', // all lower-case
-			'.Vcms-subone-subtwo-fileutils',
-			'vcmssubonesubtwofileutils',
-			'vcms\subone\subtwo\fileutils',
-			'vcms::subone::subtwo::fileutils',
-			'vcms_subone_subtwo_fileutils',
-			'vcms subone subtwo fileutils',
-			'vcms   subone   subtwo	 fileutils',
-			'.Vcms.SubOne.SubTwo.FileUtils', // Matching case
-			'.Vcms-SubOne-SubTwo-FileUtils',
-			'VcmsSubOneSubTwoFileUtils',
-			'Vcms\SubOne\SubTwo\FileUtils',
-			'Vcms::SubOne::SubTwo::FileUtils',
-			'Vcms_SubOne_SubTwo_FileUtils',
-			'Vcms SubOne SubTwo FileUtils',
-			'Vcms 	SubOne 	SubTwo	 FileUtils'
-		);
-		
-		foreach ($badNames as $badName) {
-			foreach($patterns as $pattern) {
-				$num = preg_match($pattern, $badName);
-				$this->assertIdentical(0, $num, "Should not match $badName");
-			}
-		}
+		//TODO: implement
 	}
 	
 	/*
-	 * Test file system path correction.
+	 * Test loading an external library that is set without a
+	 * class name in the main config file.
+	 * An exception is expected to be thrown. 
 	 */
-	public function testTruePath()
+	public function testLibraryWithoutClass()
 	{
-		$d = DIRECTORY_SEPARATOR;
-		
-		// test current working directory
-		$this->assertIdentical(getcwd(), Autoloader::truepath(''));
-		
-		// test . correction
-		$this->assertIdentical(getcwd().$d.'a', Autoloader::truepath('./a'));
-		$this->assertIdentical(getcwd(), Autoloader::truepath('.'));
-		$this->assertIdentical(
-			$this->tempDir,
-			Autoloader::truepath($this->tempDir.'/.')
-		);
-		$this->assertIdentical(
-			$d.'a'.$d.'b'.$d.'c.d',
-			Autoloader::truepath('/a/b/c.d')
-		);
-		
-		// test .. correction
-		$this->assertIdentical(dirname(getcwd()), Autoloader::truepath('..'));
-		$this->assertIdentical(
-			$this->tempDir,
-			Autoloader::truepath($this->tempDir.'/a/..')
-		);
-		$this->assertIdentical(
-			$this->tempDir.$d.'b'.$d.'c',
-			Autoloader::truepath($this->tempDir.'/a/../b/c/d/..')
-		);
-			
-		// test multiple directory separators
-		$this->assertIdentical(
-			$d.'a'.$d.'b'.$d.'c'.$d.'d',
-			Autoloader::truepath('/a//b///c//d/')
-		);
+		//TODO: implement
 	}
 	
 	/*
-	 * Creates and initializes a test autoloader with the temporary directory. 
+	 * Test loading an external file that is not extending AbstractLibraryInit. 
+	 * An exception is expected to be thrown. 
 	 */
-	protected function setupAutoloader()
+	public function testLibraryNotExtendingAbstractInit()
 	{
-		// Set up the autoloader
-		$autoloader = spl_autoload_register('\Vcms\Autoloader::autoload');
-		if (! $autoloader) {
-			$this->fail('Could not attach the autoloader!');
-		}
-		
-		// Add in the temp directory path
-		Autoloader::addDir($this->tempDir);
+		//TODO: implement
 	}
-	
-	/*
-	 * Writes out a simple PHP class to disk with the give class name and namespace
-	 * in the specified file path.
-	 */
-	protected function writePHPFile($namespace, $class, $filePath)
-	{
-		$phpClass = "<?php\n namespace {$namespace};\n class {$class} {}\n ?>";
-		if (file_put_contents($filePath, $phpClass, LOCK_EX) == false) {
-			$this->fail('Could not write test PHP file.');
-		}
-	}
-	
-	/*
-	 * Test autoloading a class directly in the autoloaders path.
-	 */
-	public function testFlatDirLoad()
-	{
-		$this->setupTempDir();
-		$this->setupAutoloader();
-		
-		// Create the class and test autoloading, remove the file once we are finished
-		$path = FileUtils::truepath($this->tempDir.'/vcms.testing.atest.php');
-		$this->writePHPFile('Vcms\Testing', 'ATest', $path);
-		$var = new \Vcms\Testing\ATest();
-		
-		$this->assertTrue(unlink($path));
-		$this->removeTempDir();
-	}
-	
-	/*
-	 * Test autoloading a class in a sub-directory of a directory in the autoloaders
-	 * path.
-	 */
-	public function testMultiDirLoad()
-	{
-		$this->setupTempDir();
-		$this->setupAutoloader();
-		
-		// Create a few sub-folders
-		$dirPath = FileUtils::truepath($this->tempDir.'/a/b');
-		$this->assertTrue(mkdir($dirPath, 0777, true));
-		
-		// Create the class and test autoloading, remove the file once we are finished
-		$path = Autoloader::truepath($dirPath.'/vcms.testing.atest.php');
-		$this->writePHPFile('Vcms\Testing', 'ATest', $path);
-		$var = new Vcms\Testing\ATest();
-		
-		$this->assertTrue(unlink($path));
-		$this->assertTrue(rmdir($dirPath));
-		$this->assertTrue(rmdir(dirname($dirPath)));
-		$this->removeTempDir();
-	}
+
 }
